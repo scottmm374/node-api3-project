8f3a40ba0ea4d3fde50107b56f9f80fbf4a00ef9
Michelle Scott <scottmm374@gmail.com>
2019-12-10 12:32:45 -0500

    Set up logger middleware

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
-
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
+server.use(express.json());
+server.use(logger);
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      "User-Agent"
+    )}`
+  );
+  next();
+}
+
+function validateUserId(req, res, next) {}
+
+function validateUser(req, res, next) {}
+
+function validatePost(req, res, next) {}
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
 
 module.exports = server;
