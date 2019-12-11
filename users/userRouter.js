925d7787109fa16bda456af4cbf6d65de47822a7
Michelle Scott <scottmm374@gmail.com>
2019-12-11 18:26:32 -0500

    Added checks for body

--- users/userRouter.js
+++ users/userRouter.js
@@ -105,14 +105,15 @@ function validateUserId() {
 
 function validateUser() {
   return (req, res, next) => {
+    if (Object.keys(req.body).length === 0) {
+      return res.status(400).json({
+        message: "missing user data"
+      });
+    }
+
     if (!req.body.name) {
       return res.status(400).json({
         message: "missing required name field"
-      });
-    }
-    if (!req.body) {
-      return res.status(400).json({
-        message: "missing user data"
       });
     }
 
@@ -122,16 +123,17 @@ function validateUser() {
 
 function validatePost() {
   return (req, res, next) => {
+    if (Object.keys(req.body).length === 0) {
+      return res.status(400).json({
+        message: "missing post data"
+      });
+    }
     if (!req.body.text) {
       return res.status(400).json({
         message: "missing required text field"
       });
     }
-    if (!req.body) {
-      return res.status(400).json({
-        message: "missing post data"
-      });
-    }
+
     next();
   };
 }
