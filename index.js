cf7a822fe51e48e9bae10dceca824c8108e50885
Michelle Scott <scottmm374@gmail.com>
2019-12-12 19:58:42 -0500

    Added heroku link in readme

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./server");
 const host = process.env.HOST || "0.0.0.0";
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8080;
 
 server.listen(port, host, () => {
   console.log(`Server running on ${port} - ${host}`);
